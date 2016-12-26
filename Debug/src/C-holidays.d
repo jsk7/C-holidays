a018c9adc5c6732f0ac5175c93884e0d4392bb24
Juan Ignacio Ostroski <juan.biltes@gmail.com>
2016-12-26 16:55:35 -0300

    Hola Mundo

---
+++ Debug/src/C-holidays.d
@@ -0,0 +1,1 @@
+src/C-holidays.o: ../src/C-holidays.c
---
+++ src/C-holidays.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : C-holidays.c
+ Author      : Juan Ignacio Biltes
+ Version     :
+ Copyright   : MIT
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return 1;
+}
